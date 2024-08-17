1e003a717289b7ac4f590ee3022086383cd070b0
keko333z <keko333z@gmail.com>
2024-08-17 10:43:28 -0300

    fordeploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://https://keko333z.github.io/flags/'
+  base: '/flags/'
 })
